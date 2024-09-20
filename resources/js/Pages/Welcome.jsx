5d0fb18d7c0e77d035163b511c648f8cd5eb1d73
ajn123 <alexander.norton@frontgatetickets.com>
2024-09-19 23:58:35 -0400

    install tailwind

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,8 +5,13 @@ import { Head } from "@inertiajs/react";
 
 export default function Welcome() {
     return (
-        <h1>
-            hi
-        </h1>
+        <div className=" h-screen bg-cyan-500 flex flex-col justify-center items-center">
+            <h1 className="text-6xl font-bold">
+                Welcome to Laravel React
+            </h1>
+            <p className="text-2xl">
+                This is a demo application.
+            </p>
+        </div>
     );
 }
