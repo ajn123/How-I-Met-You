a1f7fcf003609924172453656d932c3da9305991
ajn123 <alexander.norton@frontgatetickets.com>
2024-09-20 12:01:19 -0400

    configure ziggy

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,12 +4,12 @@ import '../css/app.css';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
+import {ZiggyVue} from "ziggy-js";
 createInertiaApp({
+
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
     setup({ el, App, props }) {
         if (import.meta.env.DEV) {
             createRoot(el).render(<App {...props} />);
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import * as path from "path";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: [ 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
+
     ],
 });
