0dd0fc7a55478c692701edfa3af363e09b7c12c1
ajn123 <alexander.norton@frontgatetickets.com>
2024-09-23 15:04:32 -0400

    added Bearer Token authentication test

---
+++ tests/Feature/SanctumAuthTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+
+test('Can make request acting as user', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user)->json('GET', '/api/events')
+        ->assertStatus(200);
+});
+
+test('Can NOT make request', function () {
+    $this->json('GET', '/api/events')
+        ->assertStatus(401);
+});
+
+test('can use sanctum through bearer token to make request', function () {
+    $user = User::factory()->create();
+    $this->withHeaders([
+        'Authorization' => 'Bearer ' . $user->createToken('apiToken')->plainTextToken
+    ])->json('GET', '/api/events')
+        ->assertStatus(200);
+});
