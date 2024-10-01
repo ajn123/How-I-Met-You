3f38c88ba8cad381fa705203999be6dfa985d8ee
ajn123 <ajn123@vt.edu>
2024-09-30 21:40:20 -0400

    updated bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,8 +3,5 @@ window.axios = axios;
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-window.axios.defaults.headers.common["Authorization"] =
-    `Bearer ${loadEnv().API_TOKEN};`;
 
 import "react-toastify/dist/ReactToastify.css";
-import { loadEnv } from "vite";
